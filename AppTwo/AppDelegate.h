99eba3e1baeaaeafb1e7bed7b30c0bcbf20b8931
Vinu Ilangovan <vinuilangovan@208-92-198-228.ip.ctc.biz>
2014-04-01 17:10:31 -0500

    Initial Commit

---
+++ AppTwo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppTwo
+//
+//  Created by Vinu Ilangovan on 4/1/14.
+//  Copyright (c) 2014 Vinu Ilangovan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppTwo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AppTwo
+//
+//  Created by Vinu Ilangovan on 4/1/14.
+//  Copyright (c) 2014 Vinu Ilangovan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
